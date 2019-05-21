d58c8d1b65d7cef900d814cf5cbe9905c380bd8f
HIDAYZ <HIDAYZ@HIDAYZ-KSY>
2019-05-21 19:22:59 +0900

    서브메뉴 수정중

--- jstest/WebContent/module.js
+++ jstest/WebContent/module.js
@@ -1,11 +1,26 @@
 
 	var dd = setInterval("nowtime()", 1000);
-	
+/*	var mql = window.matchMedia("screen and (max-width: 768px)");
+
+	mql.addListener(function(e) {
+	    if(e.matches) {
+	    	//alert('모바일 화면 입니다.');
+	    } else {
+	    	//alert('데스크탑 화면 입니다.');
+	    	
+	    }
+	});*/
 	$( window ).resize(function() {
-		var widhth =document.body.clientWidth;
-		var height =document.body.clientHeight;
-		document.getElementById("nowtimestring").innerHTML = "변형" + "<br>";  
+		document.getElementById("nowtimestring").innerHTML = "변형" + "<br>"; 
 		//창크기 변화 감지
+		window.onfocus=function(){
+		}
+		window.onload=function(){
+		 window.focus(); // 현재 window 즉 익스플러러를 윈도우 최상단에 위치
+		window.moveTo(0,0); // 웹 페이지의 창 위치를 0,0 (왼쪽 최상단) 으로 고정
+		window.resizeTo(1280,800); // 웹페이지의 크기를 가로 1280 , 세로 800 으로 고정(확장 및 축소)
+		window.scrollTo(0,250); // 페이지 상단 광고를 바로 볼 수 있게 스크롤 위치를 조정
+		}
 		});
 	
 	function nowtime() {
@@ -21,6 +36,8 @@
 				+ " : " + minNow.toString() + " : " + secondNow.toString();
 
 	}
+
+
 	function stoptime() {
 		console.log("확인");
 		document.getElementById("nowtimestring").innerHTML = "멈춤" + "<br>";
